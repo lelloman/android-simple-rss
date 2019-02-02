ef405805d844f37dbb2955c8d8a7dd28d03f866d
lelloman <cerasuolodomenico@gmail.com>
2019-02-02 16:51:14 +0100

    Fixed settings toggle button resetting on configuration change

--- app/src/main/java/com/lelloman/read/widget/ToggleSettingItemView.kt
+++ app/src/main/java/com/lelloman/read/widget/ToggleSettingItemView.kt
@@ -1,10 +1,14 @@
 package com.lelloman.read.widget
 
 import android.content.Context
+import android.os.Bundle
+import android.os.Parcelable
 import android.util.AttributeSet
+import android.util.SparseArray
 import android.widget.CompoundButton
 import android.widget.Switch
 import com.lelloman.read.R
+
 
 class ToggleSettingItemView
 @JvmOverloads constructor(
@@ -23,5 +27,29 @@ class ToggleSettingItemView
 
     fun setOnCheckedChanged(listener: CompoundButton.OnCheckedChangeListener) {
         switch.setOnCheckedChangeListener(listener)
+    }
+
+    override fun onSaveInstanceState(): Parcelable? = Bundle().apply {
+        putParcelable(KEY_SUPER_STATE, super.onSaveInstanceState())
+        putBoolean(KEY_IS_CHECKED, switch.isChecked)
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        val superState = if (state is Bundle) {
+            switch.isChecked = state.getBoolean(KEY_IS_CHECKED)
+            state.getParcelable(KEY_SUPER_STATE)
+        } else {
+            state
+        }
+        super.onRestoreInstanceState(superState)
+    }
+
+    override fun dispatchRestoreInstanceState(container: SparseArray<Parcelable>?) {
+        // no op
+    }
+
+    private companion object {
+        const val KEY_SUPER_STATE = "SuperState"
+        const val KEY_IS_CHECKED = "IsChecked"
     }
 }
