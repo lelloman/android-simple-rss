f30dc4f51177d65f134be7ec8b941c9a8aa1ffbf
lelloman <cerasuolodomenico@gmail.com>
2019-02-03 22:22:04 +0100

    Refactored pick theme screen in walkthrough

--- app/src/main/java/com/lelloman/simplerss/ui/walkthrough/viewmodel/ThemeListItemViewModel.kt
+++ app/src/main/java/com/lelloman/simplerss/ui/walkthrough/viewmodel/ThemeListItemViewModel.kt
@@ -1,9 +1,16 @@
 package com.lelloman.simplerss.ui.walkthrough.viewmodel
 
+import com.lelloman.common.view.AppTheme
 import com.lelloman.common.viewmodel.BaseListItemViewModel
 import com.lelloman.simplerss.ui.walkthrough.ThemeListItem
 
-class ThemeListItemViewModel : BaseListItemViewModel<Long, ThemeListItem> {
+class ThemeListItemViewModel(private val onItemClickListener: (ThemeListItem) -> Unit)
+    : BaseListItemViewModel<Long, ThemeListItem> {
+
+    private lateinit var item: ThemeListItem
+
+    var appTheme: AppTheme? = null
+        private set
 
     var name: String = ""
         private set
@@ -12,7 +19,11 @@ class ThemeListItemViewModel : BaseListItemViewModel<Long, ThemeListItem> {
         private set
 
     override fun bind(item: ThemeListItem) {
+        this.item = item
         name = item.theme.name
         isSelected = item.isEnabled
+        appTheme = item.theme
     }
+
+    fun onItemClicked() = onItemClickListener(item)
 }
