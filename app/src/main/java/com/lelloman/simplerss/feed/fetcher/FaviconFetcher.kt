8ed6ac1a103c5355d5bdaa477f9874bc660a4191
lelloman <cerasuolodomenico@gmail.com>
2019-02-09 10:29:06 +0100

    Add favicon retrieval via DuckDuckGo API

--- app/src/main/java/com/lelloman/simplerss/feed/fetcher/FaviconFetcher.kt
+++ app/src/main/java/com/lelloman/simplerss/feed/fetcher/FaviconFetcher.kt
@@ -3,6 +3,8 @@ package com.lelloman.simplerss.feed.fetcher
 import android.support.annotation.VisibleForTesting
 import com.lelloman.common.utils.UrlValidator
 import com.lelloman.simplerss.http.HttpClient
+import com.lelloman.simplerss.http.HttpRequest
+import com.lelloman.simplerss.http.HttpResponse
 import io.reactivex.Maybe
 
 class FaviconFetcher(
@@ -10,18 +12,27 @@ class FaviconFetcher(
     private val urlValidator: UrlValidator
 ) {
 
-    fun getPngFavicon(url: String): Maybe<ByteArray> = urlValidator.findBaseUrlWithoutProtocol(url)
+    fun getPngFavicon(url: String): Maybe<ByteArray> = getPngFaviconInternal(url, ::getDuckDuckGoFaviconUrl)
+        .switchIfEmpty(getPngFaviconInternal(url, ::getGoogleS2FaviconUrl))
+        .onErrorComplete()
+
+    private fun getPngFaviconInternal(
+        url: String,
+        faviconUrlProvider: (String) -> String
+    ): Maybe<ByteArray> = urlValidator
+        .findBaseUrlWithoutProtocol(url)
         .flatMapSingle { baseUrl ->
-            httpClient
-                .request(com.lelloman.simplerss.http.HttpRequest(getGoogleS2FaviconUrl(baseUrl)))
+            httpClient.request(HttpRequest(faviconUrlProvider(baseUrl)))
         }
         .filter { response ->
             response.isSuccessful && response.body.isNotEmpty()
         }
-        .map { it.body }
+        .map(HttpResponse::body)
         .onErrorComplete()
 
     @VisibleForTesting
     fun getGoogleS2FaviconUrl(baseUrl: String) = "http://www.google.com/s2/favicons?domain=$baseUrl"
 
+    @VisibleForTesting
+    fun getDuckDuckGoFaviconUrl(baseUrl: String) = "https://icons.duckduckgo.com/ip3/$baseUrl.ico"
 }
